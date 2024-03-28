d180dd84f676f5a657fe261d9bbd62a2168fd923
ryanguyen206 <ryanninja595@gmail.com>
2024-03-28 12:45:11 -0700

    configure theme colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,18 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors:{
+        "purple": "#7c5dfa",
+        "purple_light":"#9277ff",
+        "red":"#EC5757",
+        "red_light":"#ff9797",
+        "light_bg":"#F8F8FB",
+        "text-300":"#DFE3FA",
+        "text-400":"#7E88C3",
+        "text-500":"#888EB0"
+
+
+      }
     },
   },
   plugins: [],
