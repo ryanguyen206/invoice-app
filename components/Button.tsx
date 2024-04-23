1c71be45a888292b05df3b054f23783125008e8a
ryanguyen206 <ryanninja595@gmail.com>
2024-04-22 20:25:14 -0700

    made button full width on sm screens

--- components/Button.tsx
+++ components/Button.tsx
@@ -18,7 +18,7 @@ const Button : FC<ButtonProps> = ({pendingText, defaultText, ...props}) => {
           type='submit' 
           disabled={pending}  
           {...props}
-          className="uppercase flex mt-10 w-1/2 mx-auto text-center justify-center rounded-md bg-purple p-3 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-violet-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          className="uppercase flex  max-w-[250px] mt-10 md:w-1/2 w-full border mx-auto text-center justify-center rounded-md bg-purple p-3 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-violet-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
             {pending ? pendingText : defaultText}
         </button>
