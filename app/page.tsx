b408966dfc37328bebc611435395d8cd1999c44d
ryanguyen206 <ryanninja595@gmail.com>
2024-05-14 14:39:26 -0700

    small screen height should be full

--- app/page.tsx
+++ app/page.tsx
@@ -16,6 +16,8 @@ export default async function Home() {
 
   const invoices = await getInvoice(session)
 
+
+
   // const response = await fetch(`${process.env.NEXTAUTH_URL}/api/invoices`,
   // { cache: "no-cache", method: "GET", headers: headers() }
   // )
@@ -23,7 +25,7 @@ export default async function Home() {
   // const invoices : Invoice[] = await response.json()
 
   return (
-    <main className="bg-bg_light h-full container max-w-6xl mx-auto" >
+    <main className="bg-bg_light h-screen lg:h-full container max-w-6xl mx-auto" >
       <div className="mt-20 mx-10 lg:mx-20">
       <Header invoices={invoices}/>
         <Suspense fallback={<Loading/>}>
